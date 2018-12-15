3210dff41cda637a862437c941912dbe9a3aea0f
Catalina <cristea.catalina89@gmail.com>
2018-12-15 21:02:22 +0200

    am schimbat "nu are somn" cu "nu o sa adoarma curand"

--- src/first/java/Main.java
+++ src/first/java/Main.java
@@ -3,7 +3,7 @@ package first.java;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Tudor nu are somn");
+        System.out.println("Tudor nu o sa adoarma curand");
 
     }
 }
